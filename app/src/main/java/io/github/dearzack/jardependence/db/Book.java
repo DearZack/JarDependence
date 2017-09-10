3d41fd69a93694b0e74c1548eb8e84e291bafb96
DearZack <zack4work@gmail.com>
2017-09-10 17:02:58 +0800

    Initial commit

---
+++ app/src/main/java/io/github/dearzack/jardependence/db/Book.java
@@ -0,0 +1,36 @@
+package io.github.dearzack.jardependence.db;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * Created by Zack on 2017/9/8.
+ */
+@Entity
+public class Book {
+
+    @Id
+    private Long id;
+    private String name;
+    @Generated(hash = 381496039)
+    public Book(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    @Generated(hash = 1839243756)
+    public Book() {
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
